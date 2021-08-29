afb3933c6815c0b4b6a50045afc27781f8d3f001
Donald Brower <donald.brower1@gmail.com>
2021-08-29 15:01:05 -0700

    ??

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,20 @@
 var { DateTime } = require("luxon");
 var syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('css');
-  eleventyConfig.addPassthroughCopy('images');
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("images");
 
-  eleventyConfig.addPlugin(syntaxHighlight, {
-  });
+  eleventyConfig.addPlugin(syntaxHighlight, {});
 
-  eleventyConfig.addFilter("DATE_MED", function(value) {
+  eleventyConfig.addFilter("DATE_MED", function (value) {
     return DateTime.fromJSDate(value).toLocaleString(DateTime.DATE_MED);
   });
-  eleventyConfig.addFilter("DATE_HUGE", function(value) {
+  eleventyConfig.addFilter("DATE_HUGE", function (value) {
     return DateTime.fromJSDate(value).toLocaleString(DateTime.DATE_HUGE);
   });
 
   return {
-    passthroughFileCopy: true
-  }
-}
-
-
+    passthroughFileCopy: true,
+  };
+};
